84fcdb8f6eb4b1edb8f1e16cc48388790a3aa86b
ximing <ximing@meituan.com>
2016-02-21 20:55:01 +0800

    bone

--- deploy/after.sh
+++ deploy/after.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+rm -rf .bone
 git clone git@github.com:yeanzhi/blog-bone.git .bone
 node ./deploy/mv-posts.js
 cd .bone
+git clone https://github.com/litten/hexo-theme-yilia.git themes/hexo-theme-yilia
 npm install
 hexo generate
 hexo deploy
