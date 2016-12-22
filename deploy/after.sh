4dec6e4169b1718b1ca0b494b729d25df97725bd
yeanzhi <807754634@qq.com>
2016-12-22 21:32:47 +0800

    Updates deploy/after.sh
    Auto commit by GitBook Editor

--- deploy/after.sh
+++ deploy/after.sh
@@ -3,7 +3,7 @@ rm -rf .bone
 git clone git@github.com:yeanzhi/blog-bone.git .bone
 node ./deploy/mv-posts.js
 cd .bone
-git clone https://github.com/litten/hexo-theme-yilia.git themes/hexo-theme-yilia
+#git clone https://github.com/litten/hexo-theme-yilia.git themes/hexo-theme-yilia
 npm install
 hexo generate
 hexo deploy
