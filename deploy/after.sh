af3d44e7c7d805ec0c884042dcf43388a9abdb8e
yeanzhi <807754634@qq.com>
2016-12-24 00:08:39 +0800

    Updates deploy/after.sh
    Auto commit by GitBook Editor

--- deploy/after.sh
+++ deploy/after.sh
@@ -8,5 +8,6 @@ git clone https://github.com/iissnan/hexo-theme-next themes/next
 npm install
 hexo generate
 hexo deploy
+rsync -av ./public/ -e 'ssh' --progress --exclude node_modules  root@114.215.142.175:/usr/local/openresty/nginx/conf
 cd ..
 
